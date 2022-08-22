0609a1d003bf7aeb4d5a83868f08d5e78040b9ce
Jonathan Stevenson <jonathan@greatfigure8.com>
2022-08-22 13:21:48 -0500

    workingf coimmit

--- src/lib/ordsRunner.ts
+++ src/lib/ordsRunner.ts
@@ -1,48 +1,132 @@
-import axios, {AxiosError, AxiosInstance} from "axios";
+import axios, { AxiosError, AxiosInstance } from 'axios'
+import _ from 'lodash'
+import { IEntity } from './IEntity'
+import { IActionParams } from './IActionParams'
 
-export type OrdsResponse<T> = {
-    items: Array<T>;
-    hasMore: boolean,
-    limit: number,
-    offset: number,
-    count: number,
-    links: Array<OrdsLink>
-}
-export type OrdsLink = {
-    rel: string;
-    link: string;
-}
-export type Response<T> = {
-    data: OrdsResponse<T>;
-    error?: AxiosError;
+export interface OrdsResponse<T extends IEntity> {
+  items: T[]
+  hasMore: boolean
+  limit: number
+  offset: number
+  count: number
+  links: OrdsLink[]
 }
 
-export default class OrdsRunner<T> {
-    runner: AxiosInstance;
-    constructor(private baseUrl: string) {
-        this.runner = axios.create({baseURL: baseUrl});
+export interface OrdsLink {
+  rel: string
+  link: string
+}
+
+/**
+ * The interface defining what we should expect back from any call to an ORDS endpoint
+ */
+export interface IResponse<T extends IEntity> {
+  ordsResponse: OrdsResponse<T>
+  error?: AxiosError
+}
+
+export default class OrdsRunner<T extends IEntity> {
+  runner: AxiosInstance
+  constructor (private readonly baseUrl: string) {
+    this.runner = axios.create({ baseURL: baseUrl })
+  }
+
+  public static makeResponse<T>(): IResponse<T> {
+    return {
+      ordsResponse: {
+        offset: 0,
+        links: [],
+        limit: 0,
+        count: 0,
+        hasMore: false,
+        items: []
+      },
+      error: undefined
     }
-    async get(action: string): Promise<Response<T>> {
-        const resp: Response<T> = this.makeResponse();
-        try {
-            const axiosResp = await axios.get(action);
-            resp.data = axiosResp.data;
-            return resp;
-        } catch (e) {
-            resp.error = e as AxiosError;
-            return resp;
-        }
+  }
+
+  private static mapErrorToAxiosError (err: Error): AxiosError {
+    const error = new AxiosError()
+    error.message = err.message
+    error.stack = err.stack
+    error.status = 'BadRequest'
+    error.code = '400'
+    return error
+  }
+
+  /**
+   * The main get method for all ORDS entities.  Supports paging by using the offset and limit params
+   * @param {string} action
+   * @returns {Promise<Response{T}}
+   */
+  async get ({ action }: IActionParams<T>): Promise<IResponse<T>> {
+    const resp: IResponse<T> = OrdsRunner.makeResponse<T>()
+    try {
+      const axiosResp = await axios.get<OrdsResponse<T>>(action)
+      resp.ordsResponse = axiosResp.data
+      return resp
+    } catch (e) {
+      resp.error = e as AxiosError
+      return resp
     }
-    private makeResponse(): Response<T> {
-        return {
-            data: {
-                offset: 0,
-                links: [],
-                limit: 0,
-                count: 0,
-                hasMore: false,
-                items: []
-            }, error: undefined
-        }
+  }
+
+  /**
+   * This method is the main CUD method for all ORDS entities.  It posts when there is no entity to update, it puts when there is an entity to update, and
+   * it deletes if there is an id and the params indicate a delete operation.
+   * @returns {Promise<Response{{T}}
+   * @param {IActionParams} params the action and the body
+   */
+  async upsert (params: IActionParams<T>): Promise<IResponse<T>> {
+    try {
+      const { body } = { ...params }
+      if (_.isUndefined(body)) throw new Error('To put, post or delete an item, there must be a body')
+      else if ((params.delete === true) && body.id <= 0) throw new Error(`To delete an item, there must be a valid id.  The id you passed was ${body.id}`)
+      else {
+        if (params.delete === true) return await this.delete(params)
+        return body.id > 0 ? await this.put(params) : await this.post(params)
+      }
+    } catch (e) {
+      const resp = OrdsRunner.makeResponse<T>()
+      resp.error = OrdsRunner.mapErrorToAxiosError(e as Error)
+      return resp
     }
+  }
+
+  async post ({ action, body }: IActionParams<T>): Promise<IResponse<T>> {
+    const resp: IResponse<T> = OrdsRunner.makeResponse<T>()
+    try {
+      const axiosResp = await axios.post<T>(action, body)
+      resp.ordsResponse.items.push(axiosResp.data)
+      return resp
+    } catch (e) {
+      resp.error = e as AxiosError
+      return resp
+    }
+  }
+
+  async delete ({ action, body }: IActionParams<T>): Promise<IResponse<T>> {
+    const resp: IResponse<T> = OrdsRunner.makeResponse<T>()
+    try {
+      if (_.isUndefined(body)) throw new Error('To delete an item, there must be a body')
+      const axiosResp = await axios.delete<T>(`${action}/${body.id}`)
+      resp.ordsResponse.items.push(axiosResp.data)
+      return resp
+    } catch (e) {
+      resp.error = e as AxiosError
+      return resp
+    }
+  }
+
+  async put ({ action, body }: IActionParams<T>): Promise<IResponse<T>> {
+    const resp: IResponse<T> = OrdsRunner.makeResponse<T>()
+    try {
+      const axiosResp = await axios.put<T>(action, body)
+      resp.ordsResponse.items.push(axiosResp.data)
+      return resp
+    } catch (e) {
+      resp.error = e as AxiosError
+      return resp
+    }
+  }
 }
---
+++ src/tests/ordsRunner.test.ts
@@ -0,0 +1,63 @@
+import { describe } from 'mocha'
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
+import OrdsRunner, { IResponse } from '../lib/ordsRunner'
+import { expect } from 'chai'
+
+interface TestHttpResp {
+  message: string
+  id: number
+}
+
+describe('ords runner tests', () => {
+  const postEndpoint = '/post'
+  const getEndpoint = '/get'
+  const getErrorEndpoint = '/error'
+
+  const mockAdapter = new MockAdapter(axios)
+  before(() => {
+    const getResp = OrdsRunner.makeResponse<TestHttpResp>()
+    getResp.ordsResponse.items.push({ message: 'test get', id: 1 })
+    mockAdapter.onGet(getEndpoint).replyOnce(200, getResp.ordsResponse)
+    mockAdapter.onGet(getErrorEndpoint).networkError()
+
+    const postResp: TestHttpResp = { message: 'test post', id: 2 }
+    mockAdapter.onPost(postEndpoint).replyOnce(200, postResp)
+  })
+
+  it('should run a GET and return data', async () => {
+    const ordsRunner = new OrdsRunner<TestHttpResp>('none')
+    const resp = await ordsRunner.get(getEndpoint)
+    expect(resp.ordsResponse.items[0].message).to.equal('test get')
+  })
+
+  it('should run a GET and return an error', async () => {
+    const ordsRunner = new OrdsRunner<TestHttpResp>('none')
+    const resp = await ordsRunner.get(getErrorEndpoint)
+    expect(resp.error).to.not.be.undefined
+    expect(resp.error?.isAxiosError).to.be.true
+    expect(resp.error?.message).to.equal('Network Error')
+  })
+
+  it('upsert should run a POST and return a API response with one item', async () => {
+    const expected: IResponse<TestHttpResp> = OrdsRunner.makeResponse<TestHttpResp>()
+    expected.ordsResponse.items.push({ message: 'test me', id: 2 })
+
+    const runner = new OrdsRunner<TestHttpResp>('none')
+    const resp = await runner.upsert({ action: '/post', body: { message: 'test post', id: 0 } })
+
+    expect(resp.ordsResponse.items.length).to.equal(1)
+    expect(resp.ordsResponse.items[0].message).to.equal('test post')
+    expect(resp.ordsResponse.items[0].id).to.equal(2)
+  })
+
+  it('upsert should throw error if the body is undefined', async () => {
+    const expected: IResponse<TestHttpResp> = OrdsRunner.makeResponse<TestHttpResp>()
+    expected.ordsResponse.items.push({ message: 'test me', id: 2 })
+
+    const runner = new OrdsRunner<TestHttpResp>('none')
+    const resp = await runner.upsert({ action: '/post', body: undefined })
+
+    expect(resp)
+  })
+})
